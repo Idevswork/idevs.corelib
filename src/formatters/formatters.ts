55d7876c841645f2d302e03e223eca800415b24e
KlomKling <sarawut@idevs.work>
2023-02-26 06:44:03 +0700

    Re-Format code

--- src/formatters/formatters.ts
+++ src/formatters/formatters.ts
@@ -3,62 +3,62 @@ import { FormatterContext } from '@serenity-is/sleekgrid'
 import { htmlEncode } from '@serenity-is/corelib/q'
 
 export type Formatter = {
-    format(ctx: FormatterContext): string;
+  format(ctx: FormatterContext): string
 }
 
 @Decorators.registerFormatter('Idevs.ZeroToBlankFormatter')
 export class ZeroToBlankFormatter implements Formatter {
-    static format(src: string): string {
-        if (!src) {
-            return ''
-        }
-
-        const val = parseFloat(src.replace(',', ''))
-        if (val == 0) {
-            return ''
-        }
-
-        return htmlEncode(src)
+  static format(src: string): string {
+    if (!src) {
+      return ''
     }
 
-    format(ctx: FormatterContext): string {
-        return ZeroToBlankFormatter.format(ctx.value as string)
+    const val = parseFloat(src.replace(',', ''))
+    if (val == 0) {
+      return ''
     }
+
+    return htmlEncode(src)
+  }
+
+  format(ctx: FormatterContext): string {
+    return ZeroToBlankFormatter.format(ctx.value as string)
+  }
 }
 
 @Decorators.registerFormatter('Idevs.CheckboxFormatter')
 export class CheckboxFormatter implements Formatter {
-    constructor() {
-        this.cssClass = 'text-center fs-2 text-gray-1'
-        this.trueValue = 1
-        this.falseValue = 0
-        this.trueValueIcon = 'mdi mdi-checkbox-marked-outline'
-        this.falseValueIcon = 'mdi mdi-checkbox-blank-outline'
+  constructor() {
+    this.cssClass = 'text-center fs-2 text-gray-1'
+    this.trueValue = 1
+    this.falseValue = 0
+    this.trueValueIcon = 'mdi mdi-checkbox-marked-outline'
+    this.falseValueIcon = 'mdi mdi-checkbox-blank-outline'
+  }
+
+  @Decorators.option()
+  public trueValue: number | string
+
+  @Decorators.option()
+  public falseValue: number | string
+
+  @Decorators.option()
+  public cssClass: string
+
+  @Decorators.option()
+  public trueValueIcon: string
+
+  @Decorators.option()
+  public falseValueIcon: string
+
+  format(ctx: FormatterContext): string {
+    const src = ctx.value as string
+    if (src == this.trueValue.toString()) {
+      return `<i class="${this.trueValueIcon} ${this.cssClass}"></i>`
+    } else if (src == this.falseValue.toString()) {
+      return `<i class="${this.falseValueIcon} ${this.cssClass}"></i>`
+    } else {
+      return htmlEncode(src)
     }
-
-    @Decorators.option()
-    public trueValue: number | string
-
-    @Decorators.option()
-    public falseValue: number | string
-
-    @Decorators.option()
-    public cssClass: string
-
-    @Decorators.option()
-    public trueValueIcon: string
-
-    @Decorators.option()
-    public falseValueIcon: string
-
-    format(ctx: FormatterContext): string {
-        const src = ctx.value as string
-        if (src == this.trueValue.toString()) {
-            return `<i class="${this.trueValueIcon} ${this.cssClass}"></i>`
-        } else if (src == this.falseValue.toString()) {
-            return `<i class="${this.falseValueIcon} ${this.cssClass}"></i>`
-        } else {
-            return htmlEncode(src)
-        }
-    }
+  }
 }
